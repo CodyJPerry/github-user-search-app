f3f52799df54bf3e32f086280542c39a53bd20b5
Cody Perry <perrysdevelopment@gmail.com>
2023-02-07 20:48:04 -0500

    Add initial hooks with placeholder info

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import ProfileCard from './components/profilecard/ProfileCard.component';
 import './App.css';
 
 function App() {
+  const [user, setUser] = React.useState({});
+
+  React.useEffect(() => {
+    /**
+     * We need to go get information from the github
+     * Using the following API Endpoint: `https://api.github.com/users/:username`
+     * REST API Docs: https://docs.github.com/en/rest/users?apiVersion=2022-11-28
+     */
+  }, [user]);
+
+  console.log(user)
   return (
-    <div class="container">
+    <div className="container">
       <Header />
       <Search />
       <ProfileCard />
