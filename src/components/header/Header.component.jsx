6ea79362552ab30ddeb6799bcc70c6c60fb0c5a3
Cody Perry <perrysdevelopment@gmail.com>
2023-04-01 14:07:41 -0400

    Add toggle component to header

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,5 +1,12 @@
+import Toggle from "../toggle/Toggle";
+
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <h1>devfinder</h1>
+      <Toggle />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,5 +1,13 @@
+import SunIcon from "../../assets/icon-sun.svg";
+
 const Toggle = () => {
-    return <h1>Toggle</h1>
-}
+  return (
+    <div className="toggle-wrapper">
+      <span>Toggle</span>
+      {/* {SunLogo} */}
+      <img src={SunIcon} alt="Icon of a sun" />
+    </div>
+  );
+};
 
 export default Toggle;
