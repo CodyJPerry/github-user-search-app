328fffe82201debff0f1f23ab57a8ffd30f4e6f7
Cody Perry <perrysdevelopment@gmail.com>
2023-04-03 15:23:04 -0400

    Reorganize profile-card component

--- src/components/profilecard/ProfileCard.component.jsx
+++ src/components/profilecard/ProfileCard.component.jsx
@@ -19,13 +19,22 @@ const ProfileCard = ({ user }) => {
 
   return (
     <div className="profile-card">
-      <div className="avatar-wrapper">
-        <img className="profile-image" src={avatar_url} alt="Profile Avatar" />
-      </div>
-      <div className="profile-wrapper">
+      <div className="header">
+        <div className="avatar-wrapper">
+          <img
+            className="profile-image"
+            src={avatar_url}
+            alt="Profile Avatar"
+          />
+        </div>
         <h2>{name}</h2>
         <a href={url}>{`@${login}`}</a>
+        <p>{created_at}</p>
+      </div>
+      <div className="bio">
         <p>{bio}</p>
+      </div>
+      <div className="body">
         <div className="profile-highlights">
           <div className="highlight">
             <p>Repos</p>
