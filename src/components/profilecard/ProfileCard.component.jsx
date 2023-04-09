ea5fb33f4bde921b794b37dae4b78a9fda5f81f2
Cody Perry <perrysdevelopment@gmail.com>
2023-04-09 10:44:31 -0400

    Reorganize layout to allow change for lager sizes

--- src/components/profilecard/ProfileCard.component.jsx
+++ src/components/profilecard/ProfileCard.component.jsx
@@ -34,18 +34,30 @@ const ProfileCard = ({ user }) => {
 
   return (
     <div className="profile-card">
-      <div className="header">
-        <div className="avatar-wrapper">
+            <div className="avatar-wrapper">
           <img
             className="profile-image"
             src={avatar_url}
             alt="Profile Avatar"
           />
         </div>
+      <div className="content">
+      <div className="header">
+        {/* <div className="avatar-wrapper">
+          <img
+            className="profile-image"
+            src={avatar_url}
+            alt="Profile Avatar"
+          />
+        </div> */}
         <div className="details">
+          <div className="title-wrapper">
           <h2>{name}</h2>
           <a href={url}>{`@${login}`}</a>
+          </div>
+          <div className="created-at">
           <p>{`Joined ${getCreatedAtDate(created_at)}`}</p>
+          </div>
         </div>
       </div>
       <div className="bio">
@@ -88,6 +100,7 @@ const ProfileCard = ({ user }) => {
             <p>{company}</p>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
