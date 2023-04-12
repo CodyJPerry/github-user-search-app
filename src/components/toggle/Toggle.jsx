d3005fbd069e75ddd92689fbe54ab81768ac62a4
Cody Perry <perrysdevelopment@gmail.com>
2023-04-12 08:46:42 -0400

    Add moon icon and transitions

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,4 +1,7 @@
+import React from "react";
 import SunIcon from "../../assets/icon-sun.svg";
+import MoonIcon from "../../assets/icon-moon.svg";
+
 import "./toggle.styles.scss";
 
 const Toggle = ({ isDarkMode, setDarkMode}) => (
@@ -7,8 +10,17 @@ const Toggle = ({ isDarkMode, setDarkMode}) => (
       onClick={setDarkMode} 
       className="toggle"
     >
+    {isDarkMode ? (
+      <React.Fragment>
         <span>Light</span>
         <img src={SunIcon} alt="Icon of a sun" />
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+        <span>Dark</span>
+        <img src={MoonIcon} alt="Icon of a moon" />
+      </React.Fragment>
+    )}
     </div>
   </div>
 );
